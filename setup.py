3ce8767f41fc62a2eb917fbdf6235d5d553c0f47
Ben Falk <falk.ben@gmail.com>
2018-10-01 15:25:27 -0400

    tqdm has issues above this version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setuptools.setup(
         "pillow >= 1.1.6",
         "requests >= 2",
         "scikit-image",  # TODO use pillow instead
-        "tqdm >= 4",
+        "tqdm == 4.20.0",
     ],
     python_requires="~= 3.4",
     extras_require={
